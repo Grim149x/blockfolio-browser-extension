a31bfbb52d6a66cea76537b36c9f797a39f709dc
nvkrokosh <nikita.krokosh@fogstream.ru>
2018-02-08 14:16:11 +1000

    - Fixed scrolling - Small fixes errors - Removed some old code

--- js/popup.js
+++ js/popup.js
@@ -1,4 +1,4 @@
-function updatePortfolio() {
+function updatePortfolio(show_scroll=true) {
     chrome.storage.sync.get({
         apiKey: '',
         refreshInterval: 30,
@@ -7,23 +7,33 @@ function updatePortfolio() {
         var api_url = 'https://api-v0.blockfolio.com/rest/get_all_positions/' + items.apiKey;
 
         if (items.apiKey == '') {
-            $('#error-message').html('Blockfolio token is not specified<br>Please visit extension options to change it');
+            $('#error-message').html('Blockfolio token is not specified<br>Please visit extension settings to change it');
             $('#error-message').fadeIn(200);
             return;
         }
 
-        $('#spinner').show();
+        if (show_scroll){
+            $('#spinner').show();
+        }
 
         $.ajax({
             dataType: "json",
             url: api_url,
             type: 'GET',
+            timeout: 10000,
             success: function(result) {
                 $('#table-body').empty();
-                $('#total-usd').text(result['portfolio']['usdValue'].toFixed(2));
-                $('#total-btc').text(result['portfolio']['btcValue'].toFixed(2));
+                $('#total-usd').text(Number(result['portfolio']['usdValue'].toFixed(2)));
+                $('#total-btc').text(Number(result['portfolio']['btcValue'].toFixed(4)));
                 $('#ChangeFiat').text(result['portfolio']['twentyFourHourPercentChangeFiat'].toFixed(2));
                 $('#ChangeBtc').text(result['portfolio']['twentyFourHourPercentChangeBtc'].toFixed(2));
+
+                if (!result['positionList'].length){
+                    $('#error-message').html('Portfolio is empty<br>Please check specified Blockfolio token on extension settings page');
+                    $('#error-message').fadeIn(200);
+                    $('#spinner').hide(200);
+                    return;
+                }
 
                 $.each(result['positionList'].sort(
                     function(obj1, obj2) {
@@ -58,7 +68,7 @@ function updatePortfolio() {
 
                         var html_text = (
                             '<tr>' +
-                            '<td class="coin">' + '<img src="' + value['coinUrl'] + '">' + '<br/>' + value['coin'] +  '</td>' +
+                            '<td class="coin">' + '<img class="coin-icon" src="' + value['coinUrl'] + '">' + '<br/>' + value['coin'] +  '</td>' +
                             '<td class="holdings">'+ holdings_html + '</td>' +
                             '<td class="price">' + price_html +'</td>' +
                             '</tr>'
@@ -66,13 +76,16 @@ function updatePortfolio() {
 
                         $('#table-body').append(html_text);
 
-                        //$('#mainPopup').css({opacity: 0.0, visibility: "visible"}).animate({opacity: 1}, 200);
                         $('#mainPopup').fadeIn(200);
+                        $('#spinner').hide(200);
                 })
             },
+            error: function(xhr, status, error) {
+                $('#error-message').html('Blockfolio api loading error<br>' + error);
+                $('#error-message').fadeIn(200);
+                $('#spinner').hide(200);
+            },
         });
-
-        $('#spinner').hide();
     })
 }
 
@@ -82,7 +95,7 @@ function toggleFiat(){
         chrome.storage.sync.set({
             fiatMode: !items.fiatMode
         }, function (){
-            updatePortfolio()
+            updatePortfolio(false)
         })
     })
 }
